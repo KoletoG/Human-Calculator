e682981faefbb801417972c6792cc8e8f657d342
KoletoG <twindestroyersbgbg@gmail.com>
2025-03-25 14:38:29 +0200

    refactor: Moved Interfaces to a separate class

---
+++ HumanCalculator/Interfaces.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanCalculator
+{
+    interface ITime
+    {
+        List<double> Times { get; }
+        double AverageTime { get; }
+        void CalcAverageTime();
+        string ShowAverageTime();
+    }
+    interface IScore
+    {
+        ITime Time { get; }
+        byte CurrentScore { get; set; }
+        void AddTime(double seconds);
+        void AddScore();
+    }
+    interface IPlayer
+    {
+        string Name { get; }
+        IScore PlayerScore { get; }
+        string GetStats();
+    }
+    internal class Interfaces
+    {
+    }
+}
--- HumanCalculator/Program.cs
+++ HumanCalculator/Program.cs
@@ -3,26 +3,7 @@ using System.Runtime.CompilerServices;
 using static System.Formats.Asn1.AsnWriter;
 namespace HumanCalculator
 {
-    interface ITime
-    {
-        List<double> Times { get; }
-        double AverageTime { get; }
-        void CalcAverageTime();
-        string ShowAverageTime();
-    }
-    interface IScore
-    {
-        ITime Time { get; }
-        byte CurrentScore { get; set; }
-        void AddTime(double seconds);
-        void AddScore();
-    }
-    interface IPlayer
-    {
-        string Name { get; }
-        IScore PlayerScore { get; }
-        string GetStats();
-    }
+    
     internal class Program
     {
        private static Random rnd = new Random();
